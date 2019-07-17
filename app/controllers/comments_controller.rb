720a7752c6520b599b54f5ca55d23ae759627e5d
HenokZegeye <henokzegeye1990@gmail.com>
2019-07-17 15:47:27 +0300

    comments_controller modified

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,12 +20,14 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
      #@comment = Comment.new(comment_params)
-    comment = @blog.comments.new(comment_params)
+    @comment = @blog.comments.new(comment_params)
    
     if @comment.save
-      render json: comment, status: :created
+      render json: @comment, status: :created
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: {
+        error: @comment.errors.full_messages
+      }, status: :unprocessable_entity
     end
   end
 
@@ -35,6 +37,7 @@ class CommentsController < ApplicationController
     if @comment.update(comment_params)
       render json: @comment, status: :ok
     else
+      
       render json: @comment.errors, status: :unprocessable_entity
     end
   end
@@ -65,6 +68,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:name, :comment, :blog_id)
+      params.permit(:name, :comment, :blog_id)
     end
 end
