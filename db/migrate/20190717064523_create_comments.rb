22b9a19e740eb0b5e5797db79e57cc47ec191a55
HenokZegeye <henokzegeye1990@gmail.com>
2019-07-17 09:53:12 +0300

    perform migration for comment model

--- db/migrate/20190717064523_create_comments.rb
+++ db/migrate/20190717064523_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.string :name
-      t.text :comment
+      t.string :name, :null => false
+      t.text :comment, :null => false
       t.references :blog, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_143514) do
+ActiveRecord::Schema.define(version: 2019_07_17_064523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,14 @@ ActiveRecord::Schema.define(version: 2019_07_15_143514) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "comment", null: false
+    t.bigint "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
+  end
+
+  add_foreign_key "comments", "blogs"
 end
