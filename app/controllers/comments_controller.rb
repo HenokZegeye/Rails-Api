29db1edf76fcbfc8bd5f6bde98e711a9c3eacddc
HenokZegeye <henokzegeye1990@gmail.com>
2019-07-17 10:46:52 +0300

    modify comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :set_blog
   before_action :set_comment, only: [:show, :update, :destroy]
-
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    #@comments = Comment.all
+    comments = @blog.comments
+    render json: comments, status: :ok
   end
 
   # GET /comments/1
   # GET /comments/1.json
   def show
+    comment = Comment.find(params[:id])
+    render json: comment, status: :ok
   end
 
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
-
+     #@comment = Comment.new(comment_params)
+    comment = @blog.comments.new(comment_params)
+   
     if @comment.save
-      render :show, status: :created, location: @comment
+      render json: comment, status: :created
     else
       render json: @comment.errors, status: :unprocessable_entity
     end
@@ -28,7 +33,7 @@ class CommentsController < ApplicationController
   # PATCH/PUT /comments/1.json
   def update
     if @comment.update(comment_params)
-      render :show, status: :ok, location: @comment
+      render json: @comment, status: :ok
     else
       render json: @comment.errors, status: :unprocessable_entity
     end
@@ -41,9 +46,21 @@ class CommentsController < ApplicationController
   end
 
   private
+
+    def set_blog
+      @blog = Blog.find(params[:blog_id])
+      render json: {
+        error: 'not found'
+        }, status: 404 if @blog.nil?
+          
+    end
+    
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
+      render json: {
+        error: 'not found'
+      },status: 404 if @comment.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
